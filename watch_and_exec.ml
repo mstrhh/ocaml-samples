6eb33d0cd3060f11d22166d6a338fd31d1715002
strobel <kontakt@it-beratung-strobel.net>
2015-01-11 12:26:59 +0100

    doc

--- watch_and_exec.ml
+++ watch_and_exec.ml
@@ -11,6 +11,8 @@
    The executable is written with hyphens, you can change that if you want to.
 *)
 
+
+(* encapsulate the loop parameters in an object *)
 let controller = object
   val flag = ref true
   val delay = 1.0
@@ -19,32 +21,34 @@ let controller = object
   method getdelay = delay
 end 
  
-  
+
 let handler _ = controller#setstop
 
 let looper fn cmd =
    let _ = Printf.printf "watching file:     \t%s\nexecuting on change:\t%s\n%!" fn cmd
    and oldmtime = ref (Unix.time ()) in     (* if we start with current time, we will execute only on next change *)
-      try
       while controller#continue do
-      let stt = Unix.stat fn in                                               
-        if stt.Unix.st_mtime > !oldmtime       (* get modification time, ignore all execution errors*)
+        let stt = Unix.stat fn in                                               
+        if stt.Unix.st_mtime > !oldmtime       
           then
             begin
               oldmtime := stt.Unix.st_mtime;
-              try ignore (Sys.command cmd) with _ -> ()
+              try ignore (Sys.command cmd) with _ -> ()    (* ignore return code, ignore all execution errors*)
             end
           else
             try
               Thread.delay controller#getdelay
-            with Unix.Unix_error(Unix.EINTR, _, _) -> Printf.printf "\n-----  %s stopped. -----\n" Sys.executable_name ; exit 0
-      done; ()
-      with  Unix.Unix_error(Unix.EINTR, _, _) -> Printf.printf "\n-----  %s stopped. -----\n" Sys.executable_name ; exit 0
+            with Unix.Unix_error(Unix.EINTR, "select", _) -> Printf.printf "\n-----  %s stopped. -----\n" Sys.executable_name ; exit 0
+      done                                   
 (*
-   I tried to install a Ctrl-C handler to smoothly shut the loop down,
-   but the signal is received normally in Thread.delay,
-   which is evidently implemented as as Unix select call, causing a Unix.EINTR exception.
-   So the controller object is largely obsolete, unless someone manages to hit the key at the
+   I tried to install a Ctrl-C handler to set a flag and smoothly shut the loop down,
+   but the signal is received normally in Thread.delay, which is evidently implemented
+   as as Unix select call, causing a Unix.EINTR exception.
+   
+   You find the type of exceptions when testing your code, so you could start without try-catch,
+   see what exceptions are thrown, and catch them with precision.
+   
+   The controller object is largely obsolete, unless someone manages to hit the key at the
    microsecond when the loop is active. The code is left here for your edification.
 *)
 
@@ -52,10 +56,11 @@ let looper fn cmd =
 
 let main fn cmd = Sys.set_signal Sys.sigint (Sys.Signal_handle handler); looper fn cmd
 
- (* match on command line parameters and print errors. As you can see we match on an array. *)
+
+
+(* match on command line parameters and print errors. As you can see we match on an array. *)
 
 let _ = match Sys.argv with             
   | [|_; fn; cmd|] -> if not (Sys.file_exists fn) then begin Printf.eprintf "File %s not found.\n" fn; exit 1 end else main fn cmd
-  
   | _ -> Printf.eprintf "Call with arguments:\n\t1. file to watch for change\n\t2. command to execute, quoted\n"; exit 1
 
