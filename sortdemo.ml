e80f1ad35792a81ec8d52f130620a22055b73c23
strobel <kontakt@it-beratung-strobel.net>
2015-01-17 00:46:36 +0100

    bubble sort, next display it

--- sortdemo.ml
+++ sortdemo.ml
@@ -1,8 +1,5 @@
 #!/usr/bin/env ocaml
-
-
 #use "topfind";;
-
 #thread;;
 #require "graphics";;
 
@@ -38,7 +35,7 @@ let apoints = Array.init xmax (fun i -> (i, R.int ymax))
 (*  A single point is visually too small, so draw a rectangle around each point *)
 
 let pointasrect (x, y) =
-  let x' = max 0 (pred x)
+  let x' = max 0 (pred x)     (* limit lower bound, TODO upper bound, visual only, not working data *)
   and y' = max 0 (pred y) in
    G.draw_rect x' y' 2 2
 
@@ -50,6 +47,32 @@ let _ = Array.iter pointasrect apoints
 (*  we need a compare function for points, that only compares y, x is equal to the array position *)
 
 let cmpy (x, y) = Pervasives.compare y
+
+let swapcntr = ref 0  (* count number of swaps *)
+let cmpcntr  = ref 0  (* count compares *)
+
+let swap a i1 i2 =
+  let temp = a.(i1) and _ = incr swapcntr in
+    a.(i1) <- a.(i2); a.(i2) <- temp 
+
+(* do one run through the array, swapping elements as needed, return bool if swapped *)
+
+let bubble1 ar =
+  let notsorted = ref false in           (* on entry this is false *)
+  for i = 0 to (Array.length ar - 2) do  (* upper limit -1 for zero based index. -1 for swap partner *)
+    incr cmpcntr;
+    if (snd ar.(i)) >  (snd ar.(i+1)) then swap ar i (i+1); notsorted := true
+  done;
+  !notsorted
+  
+let bubbleall ar =
+  let continue = ref true in
+  while !continue do
+    continue := bubble1 ar;
+    (* display *)
+    (* delay   *)
+  done
+
 
 let _ = Thread.delay 20.0
 
