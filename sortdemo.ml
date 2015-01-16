d93b21a4fb50e2e38748671b6242042b269d6b7d
strobel <kontakt@it-beratung-strobel.net>
2015-01-16 23:29:41 +0100

    draws random points

--- sortdemo.ml
+++ sortdemo.ml
@@ -7,19 +7,44 @@
 #require "graphics";;
 
 module G = Graphics
+module R = Random
 
+(*
+    get your X11 display name. Error and exit if not found.
+*)
+let display =
+  try  Sys.getenv "DISPLAY"
+  with Not_found -> prerr_endline "X11 display not found, DISPLAY not set - can't display."; exit 1
 
-
-let _ = G.open_graph ":0" ;; 
+(*
+   draw window with default size
+*)
+let _ = G.open_graph display ;;   
 let _ = G.set_window_title "IT-Beratung Strobel";;
 
+(*
+    these functions give the logical size. The maximum index is one less.
+*)
+let xmax  = G.size_x ()
+and ymax =  G.size_y () ;;
 
-let xmax  = pred (G.size_x ())
-and ymax =  pred (G.size_y ()) ;;
+(*
+    create an array of (int, int) point values, ready to plot with G.plots
+    Array.length is xmax+1, 
+*)
+let apoints = Array.init xmax (fun i -> (i, R.int ymax))
 
-Graphics.lineto xmax ymax ;;
+(*
+    A single point is visually too small, so draw a rectangle around each point
+*)
+let pointasrect (x, y) =
+  let x' = max 0 (pred x)
+  and y' = max 0 (pred y) in
+   G.draw_rect x' y' 2 2
 
-Thread.delay 8.0;
+let _ = Array.iter pointasrect apoints
 
-exit 0
+let _ = Thread.delay 20.0
+
+let _ = exit 0
 
